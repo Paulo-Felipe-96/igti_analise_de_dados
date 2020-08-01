5c5ab61424f5198b7e535ddb768ce2008aa0c6a9
Paulo Martins <paulofelipe0896@gmail.com>
2020-08-01 01:32:22 -0300

    Operadores aritmeticos e suas variações e operações matriciais

--- operacoes_aritmeticas.py
+++ operacoes_aritmeticas.py
@@ -1,7 +1,12 @@
 """
-
 Numpy permite realizar operações aritmeticas de maneira performatica e intuitiva.
 As operações podem ser feitas "element by element" ou por matrizes.
+
+Broadcasting ou propagação:
+É um recurso que permite realizar operações aritmeticas entre arrays de diferentes
+dimensões.
+Em casos onde um array é maior que outro, virtualmente o python completa os valores
+do menor array para que a operação seja concluída com sucesso.
 
 Soma:
     +
@@ -15,6 +20,13 @@ Divisão:
 Multiplicação:
     *
     np.multiply
+
+Operações matriciais, multiplicação de matrizes:
+
+Python puro A @ B
+Numpy
+    np.dot(A,B) ou
+    A.dot(B)
 """
 import numpy as np
 
@@ -22,4 +34,3 @@ v = [2, 2]
 u = [2, 2]
 soma = np.add(v, u)
 print(soma)
-
